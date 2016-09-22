38705ddcd4bd935d34a23565fc2660d3a67dd86a
Ngahu <jamaalaraheem@gmail.com>
2016-09-22 15:13:41 +0300

    working files and folders

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    from .api_1 import api as api
+    app.register_blueprint(api, url_prefix='/api')
+    return app
---
+++ app/api_1/actions.py
@@ -0,0 +1,8 @@
+from flask import jsonify
+
+from . import api
+
+@api.route('')
+def left():
+    response = jsonify({'name':'left'})
+    return response
