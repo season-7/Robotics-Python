870aa4df42a96ab9a70a9b4f239139c047004409
johnngugi <johnngugi777@gmail.com>
2016-09-23 12:29:18 +0300

    Add more resources

--- api.py
+++ api.py
@@ -4,13 +4,38 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+tasks = {
+    'left': 'Left',
+    'right': 'Right',
+    'reverse': 'Reverse',
+    'foward': 'Foward'
+}
 
-class HelloWorld(Resource):
+
+class Foward(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return tasks['foward']
 
 
-api.add_resource(HelloWorld, '/')
+class Reverse(Resource):
+    def get(self):
+        return tasks['reverse']
+
+
+class Left(Resource):
+    def get(self):
+        return tasks['left']
+
+
+class Right(Resource):
+    def get(self):
+        return tasks['right']
+
+
+api.add_resource(Foward, '/foward')
+api.add_resource(Reverse, '/reverse')
+api.add_resource(Left, '/left')
+api.add_resource(Right, '/right')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
