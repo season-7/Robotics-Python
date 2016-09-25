b4ce97ac30b082811fb1089131e9518a847179b1
johnngugi <johnngugi777@gmail.com>
2016-09-25 13:48:27 +0300

    Add new functionality

--- api.py
+++ api.py
@@ -1,41 +1,46 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, jsonify, abort, make_response, url_for
 
 app = Flask(__name__)
-api = Api(app)
 
-tasks = {
-    'left': 'Left',
-    'right': 'Right',
-    'reverse': 'Reverse',
-    'foward': 'Foward'
-}
-
-
-class Foward(Resource):
-    def get(self):
-        return tasks['foward']
-
-
-class Reverse(Resource):
-    def get(self):
-        return tasks['reverse']
-
-
-class Left(Resource):
-    def get(self):
-        return tasks['left']
+tasks = [
+    {
+        'id': 1,
+        'description': 'left'
+    },
+    {
+        'id': 2,
+        'description': 'right'
+    },
+    {
+        'id': 3,
+        'description': 'foward'
+    },
+    {
+        'id': 4,
+        'description': 'reverse'
+    }
+]
 
 
-class Right(Resource):
-    def get(self):
-        return tasks['right']
+@app.route('/pi/api/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 
-api.add_resource(Foward, '/foward')
-api.add_resource(Reverse, '/reverse')
-api.add_resource(Left, '/left')
-api.add_resource(Right, '/right')
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_tasks', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
