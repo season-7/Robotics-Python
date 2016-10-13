fdb865cf4a38ab5ec38ace8ad2b151acd6558a73
anampiu <anampiuandrew4@gmail.com>
2016-10-13 12:00:31 +0300

    media query

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -14,39 +14,47 @@ var action = {
 };
 
 $(document).ready(function () {
-    //key press function
-    $(document).on('keydown', function (e) {
-        switch (e.keyCode) {
-            case 37:
-                action.Action(1);
-                break;
-            case 38:
-                action.Action(3);
-                break;
-            case 39:
-                action.Action(2);
-                break;
-            case 40:
-                action.Action(4);
-                break;
-        }
+    var mq = window.matchMedia("(max-device-width:800px)").matches;
+    if (mq == true) {
+        //key press function
+        $(document).on('keydown', function (e) {
+            switch (e.keyCode) {
+                case 37:
+                    action.Action(1);
+                    break;
+                case 38:
+                    action.Action(3);
+                    break;
+                case 39:
+                    action.Action(2);
+                    break;
+                case 40:
+                    action.Action(4);
+                    break;
+            }
+            console.log(mq);
+        });
+    } else {
+        //button press function
+        $('#forward').on('taphold', function () {
+            action.Action(3);
+            console.log('sup sup');
+        });
+        $('#left').on('taphold', function () {
+            action.Action(1);
+            console.log('sup sup');
+        });
+        $('#right').on('taphold', function () {
+            action.Action(2);
+            console.log('sup sup');
+        });
+        $('#reverse').on('taphold', function () {
+            action.Action(4);
+            console.log('sup sup');
 
-    });
-    //button press function
-    $('#forward').on('taphold', function () {
-        action.Action(3);
-        console.log('sup sup');
-    });
-    $('#left').on('taphold', function () {
-        action.Action(1);
-        console.log('sup sup');
-    });
-    $('#right').on('taphold', function () {
-        action.Action(2);
-        console.log('sup sup');
-    });
-    $('#reverse').on('taphold', function () {
-        action.Action(4);
-        console.log('sup sup');
-    });
+        });
+        console.log(mq);
+    }
+
+
 });
