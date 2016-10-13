e4920596bc09df700cdea5aab857b149b51f775f
johnngugi <johnngugi777@gmail.com>
2016-10-13 16:16:14 +0300

    merge request

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from . import app, auth
 from .models import User
-from flask import jsonify, make_response, url_for, g, request
+from flask import jsonify, make_response, url_for, g
 
 from publish import left, right, foward, reverse
 
@@ -89,12 +89,3 @@ def reverse_command():
     response = jsonify(tasks[3])
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-
-
-# handles gears
-@app.route('/pi/app/tasks/gear', methods=['GET'])
-# @auth.login_required
-def reverse_command():
-    gear = request.get.data
-    print gear
-    return gear
