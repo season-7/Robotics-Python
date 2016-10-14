c9819d656ffdff3aa5a5ca6407deb21abf5a03f9
EduhG <edwinnyangena@gmail.com>
2016-10-14 09:27:38 +0300

    added time functions to get and set time stamps

--- pi-drive/controls.py
+++ pi-drive/controls.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import datetime
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -27,3 +29,15 @@ def pwmPin(gpioPin):
 	return GPIO.PWM(pwmPin, 1000)
 
 
+pinA = pwmPin(forward)
+pinB = pwmPin(reverse)
+
+
+def current_time():
+	return datetime.datetime.now().time()
+
+
+def end_time(secs):
+	tm = current_time()
+	start_time = datetime.datetime(100, 1, 1, tm.hour, tm.minute, tm.seconds)
+	return sys_time + datetime.timedelta(seconds = secs)
